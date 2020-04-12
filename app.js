c763222a5cab8b2d8a27748f8f8399f08e508c18
m1burkett <60003744+m1burkett@users.noreply.github.com>
2020-04-12 11:20:28 -0400

    debugged JS

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const correctAnswers = ["B", "B", "B", "B"];
 
 const clickButton = form.addEventListener("submit", (e) => {
   e.preventDefault();
- 
 
   //we need to update score
   //need to have results populate after a half a second.
@@ -17,18 +16,21 @@ const clickButton = form.addEventListener("submit", (e) => {
     form.q3.value,
     form.q4.value,
   ];
-userAnswers.forEach((answer, index) => {
-    if(answer === correctAnswers[index]) {
-        score += 25;
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 25;
     }
-});
+  });
 
-result.classList(remove);
+  result.classList(remove);
 
-
-
- 
-  
-
-  }
+  let output = 0;
+  const timer = setInterval(() => {
+    scoreResults.innerText = `${output}%`;
+    if (output === score) {
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
 });
