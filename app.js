87322f44584e02f6836456174cbedbe767dbccfd
m1burkett <60003744+m1burkett@users.noreply.github.com>
2020-04-12 10:39:55 -0400

    adding JS and colors in CSS

---
+++ app.js
@@ -0,0 +1,4 @@
+const form = document.querySelector(".quiz");
+const result = document.querySelector("resuts");
+const scoreResults = document.querySelector("span");
+const correctAnswers = ["B", "B", "B", "B"];
