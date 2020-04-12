4a8a3afe47230f8de56b9488e69bbf27549418f2
m1burkett <60003744+m1burkett@users.noreply.github.com>
2020-04-12 11:16:05 -0400

    Added functions for JS

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 const form = document.querySelector(".quiz");
-const result = document.querySelector("resuts");
+const result = document.querySelector("results");
 const scoreResults = document.querySelector("span");
 const correctAnswers = ["B", "B", "B", "B"];
+
+const clickButton = form.addEventListener("submit", (e) => {
+  e.preventDefault();
+ 
+
+  //we need to update score
+  //need to have results populate after a half a second.
+
+  let score = 0;
+  const userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+  ];
+userAnswers.forEach((answer, index) => {
+    if(answer === correctAnswers[index]) {
+        score += 25;
+    }
+});
+
+result.classList(remove);
+
+
+
+ 
+  
+
+  }
+});
